d849e8d822e977a6e220c818d13f4ae7029cc399
Anwesh43 <anweshthecool0@gmail.com>
2021-01-25 01:31:47 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -30,3 +30,43 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawLineDotExpand(context : CanvasRenderingContext2D, scale : number) {
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const gap : number = h / lines 
+        const lSize : number = gap / lineFactor 
+        const r : number = gap / ballFactor 
+        for (var j = 0; j < lines; j++) {
+            const sf : number = ScaleUtil.sinify(sc2)
+            context.save()
+            context.translate(w / 2, h - gap * j)
+            DrawingUtil.drawLine(context, 0, -lSize * sc3, 0, -lSize * sc1)
+            DrawingUtil.drawCircle(context, (-w * 0.5 - r) * (1 - sf), gap - r, r)
+            context.restore()
+        }
+    }
+
+    static drawLDENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineDotExpand(context, scale)
+    }
+}
