d5c208df0d869e4ac8c0bf245b8f5b3b837ce749
Anwesh43 <anweshthecool0@gmail.com>
2021-01-25 01:46:48 +0530

    adding DLENode class

--- index.ts
+++ index.ts
@@ -145,3 +145,45 @@ class Animator {
         }
     }
 }
+
+class DLENode {
+
+    prev : DLENode 
+    next : DLENode 
+    state : State = new State()
+    
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DLENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLDENode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DLENode {
+        var curr : DLENode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
